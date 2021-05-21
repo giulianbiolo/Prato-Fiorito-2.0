4b9f7291b93d0355066a7b5ad546f5025b858452
Giulian Biolo <giulianbiolo@gmail.com>
2021-05-22 00:39:42 +0200

    Update Game.java

--- src/sample/gameLogic/Game.java
+++ src/sample/gameLogic/Game.java
@@ -3,22 +3,12 @@ package sample.gameLogic;
 import static sample.gameLogic.Constants.N_COLS;
 import static sample.gameLogic.Constants.N_ROWS;
 
-// [x] creazione board
-// [x] creo prima le bombe
-// [x] calcolo numeri
-// [x] gestione flag
-// [ni] ccaso numero zero
-// [ni] GAME OVER
-
 public class Game {
     private Cell[][] cells;
-
 
     public Game() {
         this.cells = new Cell[N_ROWS][N_COLS];
     }
-
-
     private void initCells() {
         for (int row = 0; row < N_ROWS; row++) {
             for (int col = 0; col < N_COLS; col++) {
@@ -26,8 +16,6 @@ public class Game {
             }
         }
     }
-
-
     private void placeBombs() {
         int placedBombs = 0;
         int row, col;
@@ -40,21 +28,15 @@ public class Game {
             placedBombs++;
         }
     }
-
-
     private void populateNumberedCells() {
         for (int row = 0; row < N_ROWS; row++) {
             for (int col = 0; col < N_COLS; col++) {
                 if (!cells[row][col].isBomb()) {
                     int value = 0;
-                    if ((row > 0 && col > 0) && cells[row - 1][col - 1].isBomb()) value++;
-                    if ((row > 0) && cells[row - 1][col].isBomb()) value++;
-                    if ((row > 0 && col < N_COLS-1 ) && cells[row - 1][col + 1].isBomb()) value++;
-                    if ((col > 0) && cells[row][col - 1].isBomb()) value++;
-                    if ((col < N_COLS-1 ) && cells[row][col + 1].isBomb()) value++;
-                    if ((row < N_ROWS-1  && col > 0) && cells[row + 1][col - 1].isBomb()) value++;
-                    if ((row < N_ROWS-1 ) && cells[row + 1][col].isBomb()) value++;
-                    if ((row < N_ROWS-1  && col < N_COLS-1 ) && cells[row + 1][col + 1].isBomb()) value++;
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            if (row + i >= 0 && row + i < N_COLS && col + j >= 0 && col + j < N_ROWS && cells[row + i][col + j].isBomb())
+                                value++;
                     cells[row][col].setValue(value);
                 }
             }
@@ -62,144 +44,41 @@ public class Game {
     }
 
     /**
+     * Se la cella è uno zero allora ne scopro i vicini e per tutti i vicini controllo se ci sono zeri nascosti e richiamo checkZeros recursivamente su di essi
      *
      * @param row
      * @param col
      */
     public void checkZeros(int row, int col) {
-        // Controllo i vicini
-        // Per ogni vicino, cerco checkZeros()
-
         if (cells[row][col].getValue().equals("0")) {
             uncoverNeighbours(row, col);
             cells[row][col].buttonPressed();
-            Cell cell;
-
-            if (row > 0 && col > 0) {
-                cell = cells[row - 1][col - 1];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if (row > 0 ) {
-                cell = cells[row - 1][col];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if (row > 0 && col < N_COLS ) {
-                cell = cells[row - 1][col + 1];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if (col > 0) {
-                cell = cells[row][col - 1];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if (col < N_COLS ) {
-                cell = cells[row][col + 1];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if (row < N_ROWS  && col > 0) {
-                cell = cells[row + 1][col - 1];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if(row < N_ROWS  ) {
-                cell = cells[row + 1][col];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
-
-            if(row < N_ROWS  && col < N_COLS ) {
-                cell = cells[row + 1][col + 1];
-                if ((cell.getValue().equals("0") && cell.isHidden())) {
-                    checkZeros(cell.getRow(), cell.getCol());
-                }
-            }
+            for (int i = -1; i <= 1; i++)
+                for (int j = -1; j <= 1; j++)
+                    if (row + i >= 0 && row + i < N_COLS && col + j >= 0 && col + j < N_ROWS && cells[row + i][col + j].getValue().equals("0") && cells[row + i][col + j].isHidden())
+                        checkZeros(row + i, col + j);
         }
-    }
-
-    private void InsertFlag(int row, int col){
-        cells[row][col].setFlagged(!cells[row][col].isFlagged());
-
-    }
-    /**
-     *
-     * @param r
-     * @param c
-     */
-    private void uncoverNeighbours1(int r, int c) {
-        for (int i = 0; i <= 2; i++)
-            for (int j = 0; j <= 2; j++)
-                if ((r + i - 1 > 0 && r - i + 1 < N_COLS) && (c + j - 1 > 0 && c + j - 1 < N_ROWS)) {
-                    if (cells[r + i - 1][c + j - 1].getValue() != "0") cells[r + i - 1][c + j - 1].buttonPressed();
-                }
-    }
-    private void uncoverNeighbours(int r, int c) {
-        int row = r;
-        int col = c;
-
-        if (row > 0 && col > 0) {
-            if (cells[row - 1][col - 1].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row-1][col-1].buttonPressed();
-        }
-
-        if (row > 0 ) {
-            if (cells[row - 1][col].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row-1][col].buttonPressed();
-        }
-
-        if (row > 0 && col < N_COLS ) {
-            if (cells[row - 1][col + 1].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row-1][col+1].buttonPressed();
-        }
-
-        if (col > 0) {
-            if (cells[row][col - 1].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row][col-1].buttonPressed();
-        }
-
-        if (col < N_COLS ) {
-            if (cells[row][col + 1].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row][col+1].buttonPressed();
-        }
-
-        if (row < N_ROWS  && col > 0) {
-            if (cells[row + 1][col - 1].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row+1][col-1].buttonPressed();
-        }
-
-        if(row < N_ROWS  ) {
-            if (cells[row + 1][col].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row+1][col].buttonPressed();
-        }
-
-        if(row < N_ROWS  && col < N_COLS ) {
-            if (cells[row + 1][col + 1].getValue() != "0" && !(cells[row-1][col-1].isFlagged())) cells[row+1][col+1].buttonPressed();
-        }
-    }
-
-
-
-    /**
-     *
-     * @return
-     */
-    public Cell[][] getBoard() {
-        return this.cells;
     }
 
     /**
+     * Ciclo per tutti i vicini controllando di non uscire dai bordi [row+i>0 && row+i<N_COLS && col+j>0 && col+j<N_ROWS]
+     * E controllando che il vicino non sia uno zero e che non sia flaggato, in tal caso richiamo buttonPressed() su quella cella
      *
-     * @return
+     * @param row
+     * @param col
      */
+    private void uncoverNeighbours(int row, int col) {
+        for (int i = -1; i <= 1; i++)
+            for (int j = -1; j <= 1; j++)
+                if (row + i >= 0 && row + i < N_COLS && col + j >= 0 && col + j < N_ROWS && cells[row + i][col + j].getValue() != "0" && !cells[row + i][col + j].isFlagged())
+                    cells[row + i][col + j].buttonPressed();
+    }
+
+    public void gameOver() {
+        System.out.println("GAME OVER");
+    }
+
+    public Cell[][] getBoard() { return this.cells; }
     public Cell[][] generateBoard() {
         initCells();
         placeBombs();
