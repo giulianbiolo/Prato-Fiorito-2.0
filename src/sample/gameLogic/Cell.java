4bf73bbe41fab252a0165162681ab14fb28c167f
Giulian Biolo <giulianbiolo@gmail.com>
2021-05-22 00:40:34 +0200

    Update Cell.java

--- src/sample/gameLogic/Cell.java
+++ src/sample/gameLogic/Cell.java
@@ -4,39 +4,39 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.effect.InnerShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Border;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 import static sample.gameLogic.Constants.BTN_HEIGTH;
 import static sample.gameLogic.Constants.BTN_WIDTH;
+import static sample.gameLogic.Constants.FONT_DIR;
 
 public class Cell {
     private final int row, col;
-    // values from 0 to 8 indicates the number of mines
-    // the number 9 indicate a mine
     private int value;
     private boolean hidden;
     private boolean flagged;
     private Button button;
-    private InnerShadow pressed_shadow_effect;
 
     /**
+     * A cell in the game grid
      *
-     * @param value
-     * @param row
-     * @param col
+     * @param value Values from 0 to 8 indicate the number of mines whilst the number 9 indicates a mine
+     * @param row   Position in y axis
+     * @param col   Position in x axis
      */
     public Cell(int value, int row, int col) {
         this.value = value;
         this.button = new Button("");
+        this.button.setFont(Font.font(FONT_DIR, FontWeight.BOLD, 16));
         this.hidden = true;
         this.flagged = false;
         this.row = row;
         this.col = col;
-        this.pressed_shadow_effect = new InnerShadow();
-        this.pressed_shadow_effect.setOffsetX(4);
-        this.pressed_shadow_effect.setOffsetY(4);
-        this.pressed_shadow_effect.setColor(Color.web("0x3b596d"));
     }
 
     public void styleButton() {
@@ -51,74 +51,65 @@ public class Cell {
     }
 
     void buttonPressed() {
-        if (getValue() == "0"){
+        if(button.getStyle().contains("-fx-background-color: #E5c29F;")) { button.setStyle("-fx-background-color: #E5c29F;"); }
+        if(button.getStyle().contains("-fx-background-color: #D7B899;")) { button.setStyle("-fx-background-color: #D7B899;"); }
+        if(button.getStyle().contains("-fx-background-color: #aad751;")) { button.setStyle("-fx-background-color: #E5c29F;"); }
+        else if(button.getStyle().contains("-fx-background-color: #b9dd77;")){ button.setStyle("-fx-background-color: #D7B899;"); }
+        if (getValue() == "0") {
             button.setText("");
-        }else{
+        } else {
+            setTextColor();
             button.setText(getValue());
         }
-        if (isBomb()) { button.setStyle("-fx-text-fill: red;"); }  //TODO: usato il colore bianco solo per debug
-        button.setEffect(pressed_shadow_effect);
+        if (isBomb()) {
+            button.setStyle("-fx-text-fill: red;");
+        }  //TODO: usato il colore bianco solo per debug
         hidden = false;
     }
 
-    /**
-     *
-     * @param game
-     */
     public void actionManager(Game game) {
-        button.setOnMouseClicked(event ->
-        {
-            if (event.getButton() == MouseButton.PRIMARY){
-                if (!(isFlagged())) {
-                    if (getValue() == "X") {
-                        System.out.println("GAME OVER");
-                        System.exit(0);
-                    }
-
+        button.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.PRIMARY) {
+                if (!isFlagged() && getValue().equals("X")) {
+                    game.gameOver();
                 }
-                    buttonPressed();
-                    game.checkZeros(row, col);
-
-            } else if (event.getButton() == MouseButton.SECONDARY){
+                buttonPressed();
+                game.checkZeros(row, col);
+            } else if (event.getButton() == MouseButton.SECONDARY) {
                 setFlagged(!(isFlagged()));
             }
         });
-       /* button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                if (!(isFlagged())){
-                    buttonPressed();
-                    game.checkZeros(row, col);
-                }
-
-            }
-        });*/
     }
 
-    public InnerShadow getPressed_shadow_effect() {
-        return pressed_shadow_effect;
+    private void setTextColor() {
+        String btnColor = "black";
+        switch (this.value) {
+            case 1: btnColor = "blue"; break;
+            case 2: btnColor = "green"; break;
+            case 3: btnColor = "red"; break;
+            case 4: btnColor = "purple"; break;
+        }
+        this.button.setStyle(this.button.getStyle() + "-fx-text-fill: " + btnColor + ";");
     }
-
 
     public boolean isBomb() {
         return this.value == 9;
     }
 
-
     public boolean isHidden() {
         return hidden;
     }
-
 
     public boolean isFlagged() {
         return flagged;
     }
 
+    // TODO: Sistemare possibilmente l'emoji del flag
     public void setFlagged(boolean flagged) {
         this.flagged = flagged;
-        if(flagged){
+        if (flagged) {
             button.setText("🚩");
-        }else{
+        } else {
             button.setText("");
         }
     }
@@ -126,7 +117,6 @@ public class Cell {
     public Button getButton() {
         return this.button;
     }
-
 
     public String getValue() {
         if (this.isBomb()) {
@@ -140,24 +130,15 @@ public class Cell {
         }
     }
 
-    /**
-     *
-     * @param value
-     */
     public void setValue(int value) {
         this.value = value;
     }
-
 
     public int getRow() {
         return this.row;
     }
 
-
     public int getCol() {
         return this.col;
     }
-
-
-
 }
